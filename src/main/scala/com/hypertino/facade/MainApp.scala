b44173727c2108485dcc1c1a2b581fa02b0d2da1
Maga Abdurakhmanov <maqdev@gmail.com>
2017-07-21 22:40:30 +0200

    - fixed entry point

--- src/main/scala/com/hypertino/facade/MainApp.scala
+++ src/main/scala/com/hypertino/facade/MainApp.scala
@@ -1,13 +1,20 @@
 package com.hypertino.facade
 
 import com.hypertino.facade.modules._
+import com.hypertino.metrics.modules.MetricsModule
 import com.hypertino.service.config.ConfigModule
-import com.hypertino.service.control.api.ServiceController
-import scaldi.Injectable
+import com.hypertino.service.control.api.{Service, ServiceController}
+import scaldi.{Injectable, Module}
+
+class MainServiceModule extends Module {
+  bind [Service]          identifiedBy 'mainService        to inject[FacadeService]
+}
 
 object MainApp extends App with Injectable {
   implicit val injector = new SystemServiceModule ::
+    new MetricsModule
     new FacadeServiceModule ::
+    new MainServiceModule ::
     new FiltersModule ::
     new RamlConfigModule ::
     ConfigModule()
