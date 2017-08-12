dfeba3767fdb4e8b742c9b4d6902b8b2a4e112f0
Maga Abdurakhmanov <maqdev@gmail.com>
2017-08-12 20:08:07 +0200

    * updated hyperbus-utils

--- src/test/scala/com/hypertino/facade/utils/ResourcePatternMatcherSpec.scala
+++ src/test/scala/com/hypertino/facade/utils/ResourcePatternMatcherSpec.scala
@@ -11,20 +11,19 @@ class ResourcePatternMatcherSpec extends FreeSpec with Matchers {
       val parameterRegularMatch = ResourcePatternMatcher.matchResource(HRL("http://a/unreliable-feed/someContent"), HRL("http://a/unreliable-feed/{content}"))
       parameterRegularMatch shouldBe Some(HRL("http://a/unreliable-feed/{content}", Obj.from("content" → "someContent")))
 
-      val parameterLongPathMatch = ResourcePatternMatcher.matchResource(HRL("http://a/reliable-feed/someContent/someDetails"), HRL("http://a/reliable-feed/{content:*}"))
-      parameterLongPathMatch shouldBe Some(HRL("http://a/reliable-feed/{content:*}", Obj.from("content" → "someContent/someDetails")))
-
-      val parameterShortPathMatch = ResourcePatternMatcher.matchResource(HRL("http://a/revault/content/abc"), HRL("http://a/revault/content/{path:*}"))
-      parameterShortPathMatch shouldBe Some(HRL("http://a/revault/content/{path:*}", Obj.from("path" → "abc")))
-
+//      val parameterLongPathMatch = ResourcePatternMatcher.matchResource(HRL("http://a/reliable-feed/someContent/someDetails"), HRL("http://a/reliable-feed/{content:*}"))
+//      parameterLongPathMatch shouldBe Some(HRL("http://a/reliable-feed/{content:*}", Obj.from("content" → "someContent/someDetails")))
+//
+//      val parameterShortPathMatch = ResourcePatternMatcher.matchResource(HRL("http://a/revault/content/abc"), HRL("http://a/revault/content/{path}"))
+//      parameterShortPathMatch shouldBe Some(HRL("http://a/revault/content/{path:*}", Obj.from("path" → "abc")))
 //      val pathToPathMatch = ResourcePatternMatcher.matchResource("/rewritten-events/root/{path:*}", Uri("/rewritten-events/{path:*}", Map("path" → "root/1")))
 //      pathToPathMatch shouldBe Some(HRL("/rewritten-events/root/{path:*}", Obj.from("path" → "1")))
 
-      val doubleSlashRequest = ResourcePatternMatcher.matchResource(HRL("http://a/revault//content/abc"), HRL("http://a/revault/content/{path:*}"))
-      doubleSlashRequest shouldBe Some(HRL("http://a/revault/content/{path:*}", Obj.from("path" → "abc")))
+      val doubleSlashRequest = ResourcePatternMatcher.matchResource(HRL("http://a/revault//content/abc"), HRL("http://a/revault/content/{path}"))
+      doubleSlashRequest shouldBe Some(HRL("http://a/revault/content/{path}", Obj.from("path" → "abc")))
 
-      val tripleSlashRequest = ResourcePatternMatcher.matchResource(HRL("http://a/revault///content/abc"), HRL("http://a/revault/content/{path:*}"))
-      tripleSlashRequest shouldBe Some(HRL("http://a/revault/content/{path:*}", Obj.from("path" → "abc")))
+      val tripleSlashRequest = ResourcePatternMatcher.matchResource(HRL("http://a/revault///content/abc"), HRL("http://a/revault/content/{path}"))
+      tripleSlashRequest shouldBe Some(HRL("http://a/revault/content/{path}", Obj.from("path" → "abc")))
     }
   }
 }
